e54d4a64a84fbdde2c7fd213048eb57b36a228a5
Shugmi Shumunov <SShumunov@gmail.com>
2021-01-20 23:50:33 -0500

    Add audio for dying, jumping, and every 100 points

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ const dino = document.getElementsByClassName('dino')[0],
 	highScoreContainer = document.getElementsByClassName('high-score')[0],
 	gameOverText = document.getElementsByClassName('game-over')[0],
 	restartButton = document.getElementsByClassName('restart-button')[0],
-	LOCAL_STORAGE_SCORE_KEY = 'dino.score';
+	LOCAL_STORAGE_SCORE_KEY = 'dino.score',
+	hitAudio = document.getElementById('hit-audio'),
+	jumpAudio = document.getElementById('jump-audio'),
+	milestoneAudio = document.getElementById('milestone-audio');
 
 let jumping = false,
 	score = 0,
@@ -43,6 +46,7 @@ function dinoJump(){
 	if (!jumping){
 		jumping = true;
 		dino.classList.add('dino-jump');
+		jumpAudio.play();
 	}
 	setTimeout(function(){
 		dino.classList.remove('dino-jump');
@@ -62,6 +66,7 @@ function checkGameState(){
 	} else if (cactusLeft <= 115 && cactusLeft >= -50 && dinoBottom <= 70){
 		gameOver = true;
 		dino.classList.add('dead');
+		hitAudio.play();
 		cactus.classList.remove('cactus-move');
 		cactus.classList.add('hidden');
 		clearInterval(gameStateTimer);
@@ -75,6 +80,10 @@ function checkGameState(){
 	} else {
 		score++;
 		scoreContainer.innerHTML = adjustedScore;
+
+		if (adjustedScore !== 0 && adjustedScore % 100 === 0){
+			milestoneAudio.play();
+		}
 	}
 }
 
